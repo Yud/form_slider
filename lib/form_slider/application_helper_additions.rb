7647702f3af5589bc933b453fe9c0b714eb3e8c4
Izhar Gavish <izharg@gmail.com>
2013-06-25 17:02:09 +0300

    helper created

--- lib/form_slider/application_helper_additions.rb
+++ lib/form_slider/application_helper_additions.rb
@@ -1,37 +1,16 @@
 require 'form_slider/slider_form_builder'
+require 'form_slider/slider_field_tag'
 
 module FormSlider
   module ApplicationHelperAdditions
-    include SliderFormBuilder
+
+    include FormSlider::SliderFormBuilder
+    include FormSlider::SliderFieldTag
 
     def slider_form_for(object, options = {}, &block)
       options[:builder] = SliderFormForBuilder
       form_for(object, options, &block)
     end
 
-    def slider_field_tag(name, value=nil, *args)
-      args = args.any? ? args.first : {}
-      args[:label_name] = args[:label] && args[:label][:name] ? args[:label][:name] : name.to_s
-      args[:label_append] = args[:label] && args[:label][:append] ? args[:label][:append] : ''
-      args[:min] ||= 1
-      args[:max] ||= 10
-      args[:color] ||= 'green'
-      value = args[:min] if value.blank?
-      output = ""
-      content_tag :div, class: 'slider-container' do
-        output << concat(slider_label(args[:label_name], args[:label_append]))
-        output << concat(content_tag :div, '', class: 'slider', 'data-max' => args[:max], 'data-min' => args[:min], 'data-color' => args[:color], 'data-append' => args[:append])
-        output << concat(text_field_tag(name, value))
-      end
-    end
-
-    def slider_label(name, append)
-      output = ""
-      content_tag :label do
-        output << concat(name+' ')
-        output << concat(content_tag(:span, '', class: 'val'))
-        output << concat(append)
-      end
-    end
   end
 end
---
+++ lib/form_slider/slider_field_tag.rb
@@ -0,0 +1,18 @@
+require 'form_slider/helper'
+
+module FormSlider
+  module SliderFieldTag
+    include FormSlider::Helper
+
+    def slider_field_tag(name, value=nil, *args)
+      args = args.any? ? args.first : {}
+      value = args[:min] if value.blank?
+      output = ""
+      content_tag :div, class: 'slider-container' do
+        output << concat( slider_label(name, args) )
+        output << concat( content_tag :div, '', slider_options(args) )
+        output << concat( text_field_tag(name, value) )
+      end
+    end
+  end
+end
